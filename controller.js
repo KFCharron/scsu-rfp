aa2dac9360d62240040771d246c9f0ace6efa3e6
KFCharron <charronkyle@gmail.com>
2014-11-12 14:27:12 -0500

    Added json serializer

--- controller.js
+++ controller.js
@@ -1,1 +1,26 @@
 console.log("JavaScript File Successfully Loaded.");
+
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+$(function() {
+    $('form').submit(function() {
+        var json = JSON.stringify($('form').serializeObject());
+        // TODO send json via http post
+        return false;
+    });
+});
